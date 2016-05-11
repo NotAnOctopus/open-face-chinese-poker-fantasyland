d7cc877e9aba4f44262662e378ae4d8ce283523a
NotAnOctopus <zapmeister999@hotmail.co.uk>
2016-05-11 23:58:34 +0100

    making it work for 14 cards
    makes it work for 14 cards as well as 13, with nicer output

--- poker fantasyland js thingy.js
+++ poker fantasyland js thingy.js
@@ -1,7 +1,52 @@
-// card format: 14=A 13=K 12=Q 11=J 10=10 09=9 etc
-// suits s,h,d,c eg. 14s ace of spades
+var cards = [];
 
-// e.g. emily_smells_bad('09s','09h','13s') = [2,9,13]
+function zipadeedoodah(thingy) { // hope nobody reads this
+  thing=thingy.slice();
+  for (z=0; z<thing.length; z++) {
+    switch(thing[z].slice(0,2)) {
+      case '14':
+        thing[z]='A'+thing[z].charAt(2);
+        break;
+      case '13':
+        thing[z]='K'+thing[z].charAt(2);
+        break;
+      case '12':
+        thing[z]='Q'+thing[z].charAt(2);
+        break;
+      case '11':
+        thing[z]='J'+thing[z].charAt(2);
+        break;
+      case '10':
+        break;
+      default:
+        thing[z]=thing[z].substring(1);
+        break;
+    }
+  }
+  return thing;
+}
+
+
+function zebra(blah) {
+  var x = document.getElementById(blah);
+  var toilet = cards.indexOf(blah);
+  if (toilet == -1) {
+    cards.push(blah);
+  }
+  else {
+    cards.splice(toilet,1);
+  }
+  document.getElementById("something").innerHTML=zipadeedoodah(cards);
+}
+
+function bleurgh() {
+  cards = [];
+  document.getElementById("something").innerHTML='';
+  document.getElementById("stuff").innerHTML='';
+}
+    
+
+
 
 function emily_smells_bad(thingy) {
   thingy.sort();
@@ -15,15 +60,20 @@ function emily_smells_bad(thingy) {
   else {
     return [0,parseInt(thingy[0].slice(0,2)),parseInt(thingy[1].slice(0,2)),parseInt(thingy[2].slice(0,2))]
   }
-};
+}
+
+
+
+
+
 
 function emily_stinks(thingy) {
   thingy.sort();
   thingy.reverse();
-  if (thingy[0].slice(0,2) == '14' && thingy[1].slice(0,2) == '13' && thingy[2].slice(0,2)== '12' && thingy[3].slice(0,2) == '11' && thingy[4].slice(0,2) == '10' && thingy[0].charAt(2) == thingy[1].charAt(2) && thingy[1].charAt(2) == thingy[2].charAt(2) && thingy[2].charAt(2) == thingy[3].charAt(2) && thingy[3].charAt(2) == thingy[4].charAt(2)) {
+  if (thingy[0].slice(0,2) == '14' && thingy[1].slice(0,2) == '13' && thingy[2].slice(0,2) == '12' && thingy[3].slice(0,2) == '11' && thingy[4].slice(0,2) == '10' && thingy[0].charAt(2) == thingy[1].charAt(2) && thingy[1].charAt(2) == thing[2].charAt(2) && thingy[2].charAt(2) == thingy[3].charAt(2) && thingy[3].charAt(2) == thingy[4].charAt(2)) {
     return [9] // Royal flush
   }
-  else if (parseInt(thingy[0].slice(0,2)) - parseInt(thingy[1].slice(0,2)) == 1 && parseInt(thingy[1].slice(0,2)) - parseInt(thingy[2].slice(0,2)) == 1 && parseInt(thingy[2].slice(0,2)) - parseInt(thingy[3].slice(0,2)) == 1 && parseInt(thingy[3].slice(0,2)) - parseInt(thingy[4].slice(0,2)) == 1 && thingy[0].charAt(2) == thingy[1].charAt(2) && thingy[1].charAt(2) == thingy[2].charAt(2) && thingy[2].charAt(2) == thingy[3].charAt(2) && thingy[3].charAt(2) == thingy[4].charAt(2)) {
+  else if (parseInt(thingy[0].slice(0,2)) - parseInt(thingy[1].slice(0,2)) == 1 && parseInt(thingy[1].slice(0,2)) - parseInt(thingy[2].slice(0,2)) == 1 && parseInt(thingy[2].slice(0,2)) - parseInt(thingy[3].slice(0,2)) == 1 && parseInt(thing[3].slice(0,2)) - parseInt(thingy[4].slice(0,2)) == 1 && thingy[0].charAt(2) == thingy[1].charAt(2) && thingy[1].charAt(2) == thingy[2].charAt(2) && thingy[2].charAt(2) == thingy[3].charAt(2) && thingy[3].charAt(2) == thingy[4].charAt(2)) {
     return [8, parseInt(thingy[0].slice(0,2))] // straight flush
   }
   else if (thingy[0].slice(0,2) == '14' && thingy[1].slice(0,2) == '05' && thingy[2].slice(0,2)== '04' && thingy[3].slice(0,2) == '03' && thingy[4].slice(0,2) == '02' && thingy[0].charAt(2) == thingy[1].charAt(2) && thingy[1].charAt(2) == thingy[2].charAt(2) && thingy[2].charAt(2) == thingy[3].charAt(2) && thingy[3].charAt(2) == thingy[4].charAt(2)) {
@@ -85,7 +135,6 @@ function emily_stinks(thingy) {
   }
 };
 
-document.getElementById("thing").innerHTML = emily_smells_bad(['04c', '05c', '05h']);
 
 function whatchamacallit(hippo, platypus) {
   var goldfish = Math.min(hippo.length, platypus.length);
@@ -99,9 +148,6 @@ function whatchamacallit(hippo, platypus) {
   }
   return true;
 };
-
-var hand = ['10s', '10h', '10c', '12h', '05h']
-var foot = ['10c', '10s', '10d']
 
 function toproyalty(thingy) {
   var toilet = emily_smells_bad(thingy);
@@ -129,48 +175,47 @@ function bottomroyalty(thingy) {
   return bottomroyaltytable[carrot[0]];
 }
 
-var cardlist = ['08s', '07d', '14s', '07h', '05c', '09d', '10c', '12s', '07c', '14h', '03h', '11h', '05h'] // 13 cards for classic ofc, do 14 later
 
 // here we go
 
 var answer = -1;
 var emily_smells_terrible = ['','',''];
-var numbers = [];
 
 // i can't believe i'm about to do this
 
 // no seriously
 
-for (a=0; a<11; a++) { // top
-  for (b=a+1; b<12; b++) {
-    for (c=b+1; c<13; c++) {
-      for (d=0; d<6; d++) { // middle
-        for (e=d+1; e<7; e++) {
-          for (f=e+1; f<8; f++) {
-            for (g=f+1; g<9; g++) {
-              for (h=g+1; h<10; h++) {
-                // for (i=0; i<6; i++) { // discard
-                  var thingything = cardlist.slice();
-                  var top = [thingything[a], thingything[b], thingything[c]];
-                  thingything.splice(c, 1);
-                  thingything.splice(b, 1);
-                  thingything.splice(a, 1);
-                  var mid = [thingything[d], thingything[e], thingything[f], thingything[g], thingything[h]];
-                  thingything.splice(h, 1);
-                  thingything.splice(g, 1);
-                  thingything.splice(f, 1);
-                  thingything.splice(e, 1);
-                  thingything.splice(d, 1);
-                  // thingything.splice(i, 1); // discard
-                  var bottom = thingything.slice();
-                  if (whatchamacallit(emily_stinks(mid), emily_smells_bad(top)) && whatchamacallit(emily_stinks(bottom), emily_stinks(mid))) {
-                    var royalty = toproyalty(top) + midroyalty(mid) + bottomroyalty(bottom);
-                    if (royalty>answer) {
-                      answer = royalty;
-                      emily_smells_terrible = [top, mid, bottom];
-                      numbers = [a,b,c,d,e,f,g,h];
-                    }
-                  // }
+function omfg(cards) { // 13 of them, takes like a second
+  for (a=0; a<11; a++) { // top
+    for (b=a+1; b<12; b++) {
+      for (c=b+1; c<13; c++) {
+        for (d=0; d<6; d++) { // middle
+          for (e=d+1; e<7; e++) {
+            for (f=e+1; f<8; f++) {
+              for (g=f+1; g<9; g++) {
+                for (h=g+1; h<10; h++) {
+                  // for (i=0; i<6; i++) { // discard
+                    var thingything = cards.slice();
+                    var top = [thingything[a], thingything[b], thingything[c]];
+                    thingything.splice(c, 1);
+                    thingything.splice(b, 1);
+                    thingything.splice(a, 1);
+                    var mid = [thingything[d], thingything[e], thingything[f], thingything[g], thingything[h]];
+                    thingything.splice(h, 1);
+                    thingything.splice(g, 1);
+                    thingything.splice(f, 1);
+                    thingything.splice(e, 1);
+                    thingything.splice(d, 1);
+                    // thingything.splice(i, 1); // discard
+                    var bottom = thingything.slice();
+                    if (whatchamacallit(emily_stinks(mid), emily_smells_bad(top)) && whatchamacallit(emily_stinks(bottom), emily_stinks(mid))) {
+                      var royalty = toproyalty(top) + midroyalty(mid) + bottomroyalty(bottom);
+                      if (royalty>answer) {
+                        answer = royalty;
+                        emily_smells_terrible = [top, mid, bottom];
+                      }
+                    // }
+                  }
                 }
               }
             }
@@ -181,6 +226,63 @@ for (a=0; a<11; a++) { // top
   }
 }
 
-console.log(emily_smells_terrible)
-console.log(answer)
-console.log(numbers)
+function omfg14(cards) { // 14 of them, takes like 20 seconds
+  for (a=0; a<12; a++) { // top
+    for (b=a+1; b<13; b++) {
+      for (c=b+1; c<14; c++) {
+        for (d=0; d<7; d++) { // middle
+          for (e=d+1; e<8; e++) {
+            for (f=e+1; f<9; f++) {
+              for (g=f+1; g<10; g++) {
+                for (h=g+1; h<11; h++) {
+                  for (i=0; i<6; i++) { // discard
+                    var thingything = cards.slice();
+                    var top = [thingything[a], thingything[b], thingything[c]];
+                    thingything.splice(c, 1);
+                    thingything.splice(b, 1);
+                    thingything.splice(a, 1);
+                    var mid = [thingything[d], thingything[e], thingything[f], thingything[g], thingything[h]];
+                    thingything.splice(h, 1);
+                    thingything.splice(g, 1);
+                    thingything.splice(f, 1);
+                    thingything.splice(e, 1);
+                    thingything.splice(d, 1);
+                    thingything.splice(i, 1); // discard
+                    var bottom = thingything.slice();
+                    if (whatchamacallit(emily_stinks(mid), emily_smells_bad(top)) && whatchamacallit(emily_stinks(bottom), emily_stinks(mid))) {
+                      var royalty = toproyalty(top) + midroyalty(mid) + bottomroyalty(bottom);
+                      if (royalty>answer) {
+                        answer = royalty;
+                        emily_smells_terrible = [top, mid, bottom];
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+// you can speed this up by a decent amount if you do the discard after checking that the middle row doesn't foul the top. will do that later.
+
+
+function thingything(thing) {
+  if ((thing.length != 13) && (thing.length != 14)) {
+    document.getElementById("stuff").innerHTML='needs to be 13 cards for standard ofc or 14 for pineapple';
+  }
+  else if (thing.length == 13) {
+    omfg(cards);
+    document.getElementById("stuff").innerHTML='top '+zipadeedoodah(emily_smells_terrible[0])+'<br>'+'mid '+zipadeedoodah(emily_smells_terrible[1]) +'<br>'+'bottom '+zipadeedoodah(emily_smells_terrible[2])+'<br>'+'total royalty is '+answer;
+  }
+  else {
+    omfg14(cards);
+    document.getElementById("stuff").innerHTML='top '+zipadeedoodah(emily_smells_terrible[0])+'<br>'+'mid '+zipadeedoodah(emily_smells_terrible[1]) +'<br>'+'bottom '+zipadeedoodah(emily_smells_terrible[2])+'<br>'+'total royalty is '+answer;
+  }
+  cards=[];
+  answer = -1;
+  emily_smells_terrible = ['','',''];
+}
